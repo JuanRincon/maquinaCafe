4bde6d9695da9990030d568ad489d760a211fbf9
root <root@DESKTOP-OJS0JKN.localdomain>
2022-06-02 17:28:42 -0500

    código base de datos antes de enlazar con sus respectivas botones

--- MaqCafe/DB/maqCafe.py
+++ MaqCafe/DB/maqCafe.py
@@ -56,6 +56,77 @@ def delete_bebidas():
         print("bebida Eliminada")
         return True
     except Error as e:
-        pass
+        print("Error Deleting bebida: " + str(e))
     finally:
-        pass
+        if conn:
+            cur.close()
+            conn.close()
+
+def sellect_all_bebida():
+    conn = create:connection()
+    sql = "SELECT * FROM bebida"
+
+    try:
+        cur =cursor()
+        cur.execute(sql)
+        books = cur.fetchall()
+        return bebida
+    except Error as e:
+        print("Error selecting all bebidas: " + str(e))
+    finally:
+        if conn:
+           cur.close()
+           conn.close()
+
+def select_book_by_id(_id):
+    conn= create_conecction()
+    sql = f"SELECT * FROM bebida WHERE Id_bebida * {_id}"
+
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        bebida = cur.fetchone()
+        return bebida
+    except Error as e:
+        print("Error selecting bebida by id: " + str(e))
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+def select_bebida_by_title(title):
+    conn = create_connection()
+    sql = f"SLECT * FROM bebida WHERE title LIKE '%title%'"
+
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        book = cur.fetchall()
+        return debida
+    except Error as e:
+        print("Error selecting book by title: " + str(e))
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+
+def select_book_by_category(category):
+    conn= create_connection()
+    sql = "SELECT * FROM bebida WHERE category LIKE '%category%'"
+
+    try:
+        cur = conn.cursor()
+        cur.execute(sql)
+        books = cur.fetchall()
+        return bebida
+    except Error as e:
+        print("Error Selecting book by category: " + str(e))
+    finally:
+        if conn:
+            cur.close()
+            conn.close()
+            
+
+
+
+
